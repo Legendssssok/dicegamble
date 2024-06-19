c3074e8a15ac4b945e5f627cacbbc9f7beaa1a1f
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-19 17:55:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def gameplay(event):
                 event.chat_id,
                 f"**{user.first_name}**, it's your turn! Send a dice emoji: 🎲",
             )
-            response = await client.on(
+            response = client.on(
                 events.NewMessage(incoming=True, from_users=event.sender_id)
             )
             player1 = response.media.value

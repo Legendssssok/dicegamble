c10b1d9d12101241b626abafbc68ad5ee5d19d16
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-20 11:42:02 +0000

    pylint: auto fixes

--- main.py
+++ main.py
@@ -373,7 +373,9 @@ async def gameplay(event):
         await asyncio.sleep(3)
         await event.reply(f"Now it's {opponent_id}'s turn")
         async with client.conversation(event.chat_id) as conv:
-            await conv.wait_for_event(events.NewMessage(incoming=True, from_users=int(opponent_id)))
+            await conv.wait_for_event(
+                events.NewMessage(incoming=True, from_users=int(opponent_id))
+            )
         await asyncio.sleep(3)
         player_turn[event.sender_id] = opponent_id
         player_turn[opponent_id] = opponent_id

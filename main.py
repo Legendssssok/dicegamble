7571aab8358e2345c7386959238b44fe0a5f112b
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-21 16:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -399,8 +399,9 @@ async def gameplay(event):
     if event.text:
         return
     if event.media:
-        if not event.media.emoticon == "🎲":
-            return
+        if event.media.emoticon:
+            if not event.media.emoticon == "🎲":
+                return
     if event.sender_id in last_message_times:
         max_time = 9
         time_since_last_message = time.time() - last_message_times[event.sender_id]

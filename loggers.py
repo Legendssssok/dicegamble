135fd0bec829216862bfad82e62ead6d42c9697f
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-22 07:41:01 +0530

    Create loggers.py

---
+++ loggers.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(levelname)s] [%(name)s] : %(message)s",
+    level=logging.ERROR,
+    datefmt="%H:%M:%S",
+)
+
+
+LOGS = logging.getLogger("Dice bot")

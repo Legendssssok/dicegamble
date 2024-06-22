1e1047a775cf5973cafb90ffd7e44c19afa5320c
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-22 07:55:48 +0530

    Create gamemode.py

---
+++ database/gamemode.py
@@ -0,0 +1,12 @@
+from .all_db import legend_db
+
+def game_mode():
+  return legend_db.get_key("GAMEMODE") or {}
+
+def add_game_mode(user_id, mode, round, query_id=False):
+  ok = game_mode()
+  if query_id=false:
+    ok[user_id] = [mode, round]
+  else:
+    ok[user_id)] = [mode, round, query_id]
+  return await legend_db.set_key("GAMEMODE", ok)

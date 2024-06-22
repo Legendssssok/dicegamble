2fd4d1a521dbccc33cfd5ed083d9ea74562d3267
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-22 08:06:56 +0530

    Update gamemode.py

--- database/gamemode.py
+++ database/gamemode.py
@@ -11,10 +11,10 @@ def add_game_mode(user_id, mode, round, query_id=False):
         ok[user_id] = [mode, round]
     else:
         ok[user_id] = [mode, round, query_id]
-    return await legend_db.set_key("GAMEMODE", ok)
+    return legend_db.set_key("GAMEMODE", ok)
 
 
 def remove_game_mode(user_id):
     ok = game_mode()
     ok.pop(user_id)
-    return await legend_db.set_key("GAMEMODE", ok)
+    return legend_db.set_key("GAMEMODE", ok)

843f53b7fb3737ee838c3bf6bab99e93cf065481
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-22 07:43:24 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+REDIS_URI = os.environ.get("REDIS_URI", None)
+REDISHOST = os.environ.get("REDISHOST", None)
+MONGO_URI = os.environ.get("MONGO_URI", None)
+DATABASE_URL = os.environ.get("DATABASE_URL", None)

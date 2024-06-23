4c962dc3d2b14869d1ed3bc0da5ff6a2d6cf712a
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 03:37:51 +0530

    Create translate.py

---
+++ strings/translate.py
@@ -0,0 +1,73 @@
+import request
+
+import json
+
+
+import random
+
+import json
+import math
+import os
+import random
+import re
+import secrets
+import ssl
+from io import BytesIO
+from json.decoder import JSONDecodeError
+from traceback import format_exc
+
+
+def _package_rpc(text, lang_src="auto", lang_tgt="auto"):
+    GOOGLE_TTS_RPC = ["MkEWBc"]
+    parameter = [[text.strip(), lang_src, lang_tgt, True], [1]]
+    escaped_parameter = json.dumps(parameter, separators=(",", ":"))
+    rpc = [[[random.choice(GOOGLE_TTS_RPC), escaped_parameter, None, "generic"]]]
+    espaced_rpc = json.dumps(rpc, separators=(",", ":"))
+    freq = "f.req={}&".format(quote(espaced_rpc))
+    return freq
+
+
+def translate(*args, **kwargs):
+    headers = {
+        "Referer": "https://translate.google.co.in",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/47.0.2526.106 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    x = requests.post(
+        "https://translate.google.co.in/_/TranslateWebserverUi/data/batchexecute",
+        headers=headers,
+        data=_package_rpc(*args, **kwargs),
+    ).text
+    response = ""
+    data = json.loads(json.loads(x[4:])[0][2])[1][0][0]
+    subind = data[-2]
+    if not subind:
+        subind = data[-1]
+    for i in subind:
+        response += i[0]
+    return response
+
+
+
+def safe_load(file, *args, **kwargs):
+    if isinstance(file, str):
+        read = file.split("\n")
+    else:
+        read = file.readlines()
+    out = {}
+    for line in read:
+        if ":" in line:  # Ignores Empty & Invalid lines
+            spli = line.split(":", maxsplit=1)
+            key = spli[0].strip()
+            value = _get_value(spli[1])
+            out.update({key: value or []})
+        elif "-" in line:
+            spli = line.split("-", maxsplit=1)
+            where = out[list(out.keys())[-1]]
+            if isinstance(where, list):
+                value = _get_value(spli[1])
+                if value:
+                    where.append(value)
+    return out

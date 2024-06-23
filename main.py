180b60e36bbaedc7e2d0ffdf39cd8b39be262c0c
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-23 07:00:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1060,7 +1060,7 @@ async def check_crypto_payments():
 
 scheduler.add_job(check_upi_payments, "interval", minutes=5)
 scheduler.add_job(check_crypto_payments, "interval", minutes=5)
-
+scheduler.start()
 
 # ==================== Start Client ==================#
 

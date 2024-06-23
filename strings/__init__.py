290f82fd141ab194e926acbd1cb512849b887fbf
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-23 22:49:42 +0000

    pylint: auto fixes

--- strings/__init__.py
+++ strings/__init__.py
@@ -3,10 +3,11 @@ import sys
 from glob import glob
 from typing import Any, Dict, List, Union
 
+from translate import translate
+
 from database.all_db import legend_db
 from database.languages import *
 from loggers import LOGS
-from translate import translate
 
 try:
     from yaml import safe_load
@@ -14,14 +15,17 @@ except ModuleNotFoundError:
     LOGS.error("Please install PyYAML: pip install PyYAML")
     sys.exit(1)
 
+
 class ULTConfig:
     lang = "en"
     thumb = "resources/extras/ultroid.jpg"
+
 
 ULTConfig.lang = legend_db.get_key("language") or os.getenv("LANGUAGE", "en")
 
 languages = {}
 PATH = "strings/strings/{}.yml"
+
 
 def load(file):
     if not file.endswith(".yml"):
@@ -36,12 +40,15 @@ def load(file):
         LOGS.info(f"Error in {file[:-4]} language file")
         LOGS.exception(er)
 
+
 def load_all_languages():
     for file in glob("strings/strings/*.yml"):
         load(file)
 
+
 load(PATH.format(ULTConfig.lang))
 load_all_languages()
+
 
 def get_string(key: str, user_id: int, _res: bool = True) -> Any:
     lang = get_user_lang(user_id) or "en"
@@ -72,10 +79,12 @@ def get_string(key: str, user_id: int, _res: bool = True) -> Any:
         print("lol")
         return languages["en"].get(key) or f"Failed to load language string '{key}'"
 
+
 def get_help(key, user_id):
     doc = get_string(f"help_{key}", user_id, _res=False)
     if doc:
         return get_string("cmda", user_id) + doc
+
 
 def get_languages() -> Dict[str, Union[str, List[str]]]:
     load_all_languages()

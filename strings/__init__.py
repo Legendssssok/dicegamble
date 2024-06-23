d6b81ba9f3cbc4c4ab52d76096cbb19ae116b418
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 04:19:29 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -4,26 +4,24 @@ from glob import glob
 from typing import Any, Dict, List, Union
 
 from database.all_db import legend_db
+from database.languages import *
 from loggers import LOGS
-
-from .translate import translate
+from translate import translate
 
 try:
     from yaml import safe_load
 except ModuleNotFoundError:
-    from .translate import safe_load
-
+    LOGS.error("Please install PyYAML: pip install PyYAML")
+    sys.exit(1)
 
 class ULTConfig:
     lang = "en"
     thumb = "resources/extras/ultroid.jpg"
 
-
 ULTConfig.lang = legend_db.get_key("language") or os.getenv("LANGUAGE", "en")
 
 languages = {}
 PATH = "strings/strings/{}.yml"
-
 
 def load(file):
     if not file.endswith(".yml"):
@@ -32,22 +30,25 @@ def load(file):
         file = PATH.format("en")
     code = file.split("/")[-1].split("\\")[-1][:-4]
     try:
-        languages[code] = safe_load(
-            open(file, encoding="UTF-8"),
-        )
+        with open(file, encoding="UTF-8") as f:
+            languages[code] = safe_load(f)
     except Exception as er:
         LOGS.info(f"Error in {file[:-4]} language file")
         LOGS.exception(er)
 
+def load_all_languages():
+    for file in glob("strings/strings/*.yml"):
+        load(file)
 
 load(PATH.format(ULTConfig.lang))
+load_all_languages()
 
-
-def get_string(key: str, _res: bool = True) -> Any:
-    lang = ULTConfig.lang or "en"
+def get_string(key: str, user_id: int, _res: bool = True) -> Any:
+    lang = get_user_lang(user_id) or "en"
     try:
         return languages[lang][key]
     except KeyError:
+        print("Hello")
         try:
             en_ = languages["en"][key]
             tr = translate(en_, lang_tgt=lang).replace("\ N", "\n")
@@ -68,18 +69,16 @@ def get_string(key: str, _res: bool = True) -> Any:
             LOGS.exception(er)
         if not _res:
             return None
+        print("lol")
         return languages["en"].get(key) or f"Failed to load language string '{key}'"
 
-
-def get_help(key):
-    doc = get_string(f"help_{key}", _res=False)
+def get_help(key, user_id):
+    doc = get_string(f"help_{key}", user_id, _res=False)
     if doc:
-        return get_string("cmda") + doc
-
+        return get_string("cmda", user_id) + doc
 
 def get_languages() -> Dict[str, Union[str, List[str]]]:
-    for file in glob("strings/strings/*yml"):
-        load(file)
+    load_all_languages()
     return {
         code: {
             "name": languages[code]["name"],

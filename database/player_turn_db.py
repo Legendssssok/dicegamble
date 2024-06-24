cf4735f9b8306b18949a214ea0d6986f0e3c732e
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 12:08:18 +0000

    pylint: auto fixes

--- database/player_turn_db.py
+++ database/player_turn_db.py
@@ -1,12 +1,15 @@
 from .all_db import legend_db
 
+
 def get_player_turn():
     return legend_db.get_key("PLAYER") or {}
+
 
 def add_player_turn(player1, player2):
     ok = get_player_turn()
     ok[player1] = player2
     return legend_db.set_key("PLAYER", ok)
+
 
 def remove_player_turn(user_id):
     ok = get_player_turn()

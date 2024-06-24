16df793d3bfd68068ef250805664ba8d4258cc04
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 17:28:58 +0530

    Create count_round_db.py

---
+++ database/count_round_db.py
@@ -0,0 +1,15 @@
+from .all_db import legend_db
+
+def get_count_round():
+    return legend_db.get_key("ROUND") or {}
+
+def add_count_round(user_id, round):
+    ok = get_count_round()
+    ok[user_id] = round
+    return legend_db.set_key("ROUND", ok)
+
+def remove_count_round(user_id):
+    ok = get_count_round()
+    ok.pop(user_id)
+    return legend_db.set_key("ROUND", ok)
+

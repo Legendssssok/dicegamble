5d30a19e886465ebfd61c8bdf01478f265b3b6f2
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 17:17:03 +0530

    Create score_db.py

---
+++ database/score_db.py
@@ -0,0 +1,9 @@
+from .all_db import legend_db
+
+def get_all_score():
+  return legend_db.get_key("SCORE") or {}
+
+def add_score(user_id, player1, player2):
+  ok = get_all_score()
+  ok[user_id] = [player1, player2]
+  return legend_db.set_key('SCORE', ok)

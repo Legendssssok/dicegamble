3965e830c4d9077c9f8d5cdfe4f6c0800262f1b5
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 12:58:59 +0000

    pylint: auto fixes

--- database/players_balance_db.py
+++ database/players_balance_db.py
@@ -1,5 +1,6 @@
 from .all_db import legend_db
 
+
 def get_players_balance():
     return legend_db.get_key("PLAYERS_BALANCE") or {}
 
@@ -8,4 +9,3 @@ def add_players_balance(user_id, balance):
     ok = get_players_balance()
     ok[user_id] = balance
     return legend_db.set_key("PLAYERS_BALANCE", ok)
-

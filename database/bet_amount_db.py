489fb7daaa331a04f817f031f803d84ad862e064
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-24 23:57:48 +0000

    pylint: auto fixes

--- database/bet_amount_db.py
+++ database/bet_amount_db.py
@@ -1,10 +1,11 @@
 from .all_db import legend_db
 
+
 def get_bet_amount():
     return legend_db.get_key("BET_AMOUNT") or {}
+
 
 def add_bet_amount(user_id, amount):
     ok = get_bet_amount()
     ok[user_id, amount]
     return legend_db.set_key("BET_AMOUNT", ok)
-

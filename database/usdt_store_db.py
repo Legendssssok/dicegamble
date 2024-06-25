16ef0b49365c7a1b9c2632dc7270fffb3ba0c706
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-25 00:02:26 +0000

    pylint: auto fixes

--- database/usdt_store_db.py
+++ database/usdt_store_db.py
@@ -1,14 +1,34 @@
 from .all_db import legend_db
 
+
 def get_usdt_store():
     return legend_db.get_key("USDT") or {}
+
 
 def remove_usdt_store(user_id):
     ok = get_usdt_store()
     ok.pop(user_id)
     return legend_db.set_key("USDT_STORE", ok)
 
-def add_usdt_store(user_id,  transaction_amount, transaction_address, transaction_timeout, transaction_checkout_url, transaction_qrcode_url, transaction_id, time):
+
+def add_usdt_store(
+    user_id,
+    transaction_amount,
+    transaction_address,
+    transaction_timeout,
+    transaction_checkout_url,
+    transaction_qrcode_url,
+    transaction_id,
+    time,
+):
     ok = get_usdt_store()
-    ok[user_id] =  [transaction_amount, transaction_address, transaction_timeout, transaction_checkout_url, transaction_qrcode_url, transaction_id, time]
+    ok[user_id] = [
+        transaction_amount,
+        transaction_address,
+        transaction_timeout,
+        transaction_checkout_url,
+        transaction_qrcode_url,
+        transaction_id,
+        time,
+    ]
     return legend_db.set_key("USDT_STORE", ok)

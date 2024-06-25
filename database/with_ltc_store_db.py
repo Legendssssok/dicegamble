c54db06e309b07ff87a6d93e58b4892113a71333
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-25 00:03:00 +0000

    pylint: auto fixes

--- database/with_ltc_store_db.py
+++ database/with_ltc_store_db.py
@@ -1,5 +1,6 @@
 from .all_db import legend_db
 
+
 def get_with_ltc_store():
     return legend_db.get_key("WITH_LTC_STORE") or {}
 
@@ -8,6 +9,7 @@ def remove_with_ltc_store(user_id):
     ok = get_with_ltc_store()
     ok.pop(user_id)
     return legend_db.set_key("WITH_LTC_STORE", ok)
+
 
 def add_with_ltc_store(user_id, transaction_amount, transaction_id):
     ok = get_with_ltc_store()

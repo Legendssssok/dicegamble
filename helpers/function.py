5f9c5232cc3cb18480cff57a8d97a7d8b47d226b
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-06-30 23:45:14 +0530

    Add files via upload

--- helpers/function.py
+++ helpers/function.py
@@ -1,4 +1,12 @@
+
+
+from hashlib import new
+from telethon import crypto
+
 from pyCoinPayments import CryptoPayments
+from decimal import Decimal, getcontext
+
+getcontext().prec = 8
 
 api_key = "rzp_live_OH4h4RtCPQFnLG"
 api_secret = "CPEWuysDiY69CIdZeDwazbdi"
@@ -9,12 +17,12 @@ IPN_URL = "https://google.com/ipn"
 
 crypto_client = CryptoPayments(API_KEY, API_SECRET, IPN_URL)
 
-
-def conversion(convert_1, convert_2, amount):
+def conversion(convert_1, convert_2, amount): 
     params = {"cmd": "rates", "accepted": 1}
     rate = crypto_client.rates(params)
     from_rate = rate[convert_1]["rate_btc"]
     to_rate = rate[convert_2]["rate_btc"]
-    conversion_rate = float(from_rate) / float(to_rate)
-    new_currency_balance = str(conversion_rate * float(amount))[:10]
+    conversion_rate = Decimal(from_rate) / Decimal(to_rate)
+    new_currency_balance = conversion_rate * Decimal(float(amount))
     return float(new_currency_balance)
+
